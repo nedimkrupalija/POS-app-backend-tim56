e08d2ea0d2ae7d572dba4ecabb0edd78a0c0a7ee
kenankd <kdizdarevi1@etf.unsa.ba>
2024-03-22 17:13:06 +0100

    implement generation of jwt

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = require('../constants.js').JWT_SECRET
 
-module.exports = (req, res, next) => {
+const verifyJWT= (req, res, next) => {
     try {
         const token = req.headers["Authorization"]
         const decoded = jwt.verify(token, JWT_SECRET)
         if (req.userData.role === 'admin') {
             const extendedToken = jwt.sign({ ...decoded, exp: Math.floor(Date.now() / 1000) + (30 * 60) }, JWT_SECRET)
+            console.log(extendedToken);
             req.headers["Authorization"] = extendedToken
         }
         req.userData = decoded
@@ -18,4 +19,8 @@ module.exports = (req, res, next) => {
         })
     }
 }
+
+
+module.exports = verifyJWT;
+
 
