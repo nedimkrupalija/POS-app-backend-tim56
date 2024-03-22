6eb1df906df106b5ddce873cd8c658065c8ae3ab
kenankd <kdizdarevi1@etf.unsa.ba>
2024-03-22 16:25:50 +0100

    implement verify-jwt middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = require('../constants.js').JWT_SECRET
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers["Authorization"]
+        const decoded = jwt.verify(token, JWT_SECRET)
+        if (req.userData.role === 'admin') {
+            const extendedToken = jwt.sign({ ...decoded, exp: Math.floor(Date.now() / 1000) + (30 * 60) }, JWT_SECRET)
+            req.headers["Authorization"] = extendedToken
+        }
+        req.userData = decoded
+        next()
+    } catch (error) {
+        return res.status(401).json({
+        message: 'Auth failed'
+        })
+    }
+}
+
