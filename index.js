5997ae49e73070bff54dd820362d043f4c2ac389
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-03-22 11:09:17 +0100

    change index.js for deployment

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}) 
+
+
+module.exports = app;
