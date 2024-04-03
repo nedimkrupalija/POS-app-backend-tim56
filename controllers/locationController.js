6014094261a976ec1d6a0af078cdcd9ce1376062
scomor55 <scomor2@etf.unsa.ba>
2024-04-03 21:37:19 +0200

    location id added and pos included

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,10 +1,28 @@
 const db = require('../config/db.js');
 
 const Location = db.location
+const POS = db.pos
 
 async function getLocations(req,res){
     try {
-        const locations = await Location.findAll();
+        const locations = await Location.findAll({
+            include: POS
+        });
+        res.json(locations);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+async function getLocationsUnique(req,res){
+    const {id} = req.params
+    try {
+        const locations = await Location.findByPk(id,{
+            include: POS
+        });
+        if(!locations){
+            return res.status(404).json({message: 'Location not found'});
+        }
         res.json(locations);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -46,4 +64,4 @@ async function deleteLocation(req,res){
     }
 }
 
-module.exports = {getLocations,createLocation,updateLocation,deleteLocation};
+module.exports = {getLocations,getLocationsUnique,createLocation,updateLocation,deleteLocation};
