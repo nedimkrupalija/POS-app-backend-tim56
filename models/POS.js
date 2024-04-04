a382289dcc712b6226adc03abca35695eedc9e85
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-04-04 16:57:57 +0200

    fix pos db

--- models/POS.js
+++ models/POS.js
@@ -4,9 +4,7 @@ const Sequelize = require("sequelize");
 module.exports = function(sequelize,DataTypes){
     const POS = sequelize.define("POS",{
         name:Sequelize.STRING,
-        status:Sequelize.STRING,
-        openingTime:Sequelize.DATE,
-        closingTime:Sequelize.DATE,
+        status:Sequelize.STRING
     })
     return POS;
 };
