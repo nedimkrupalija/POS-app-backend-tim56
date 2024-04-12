a8b9ebddb8eea342d4b2094d1773b7b6eaa61900
zlendo1 <zlendo1@etf.unsa.ba>
2024-04-12 22:57:18 +0200

    db.js readability change

--- config/db.js
+++ config/db.js
@@ -1,23 +1,25 @@
 const Sequelize = require('sequelize')
 const mysql2 = require('mysql2');
+
 const sequelize = new Sequelize("db_aa6c5b_sipos","aa6c5b_sipos","sipostim56",{host:"MYSQL6008.site4now.net",dialect:"mysql",dialectModule:mysql2,logging:false});
-const db={};
-const path = require('path');
+
+const db = {};
 
 db.Sequelize = Sequelize;  
 db.sequelize = sequelize;
-db.user = require('../models/User.js')(sequelize,Sequelize);
-db.item = require('../models/Item.js')(sequelize,Sequelize);    
-db.orderItem = require('../models/OrderItem.js')(sequelize,Sequelize);
-db.storage = require('../models/Storage.js')(sequelize,Sequelize);
-db.order = require('../models/Order.js')(sequelize,Sequelize);
-db.pos = require('../models/POS.js')(sequelize,Sequelize);
-db.location = require('../models/Location.js')(sequelize,Sequelize);
-db.storageItem = require('../models/StorageItem.js')(sequelize,Sequelize);
-db.table = require('../models/Table.js')(sequelize,Sequelize);
-db.vat = require('../models/VAT.js')(sequelize,Sequelize);
-db.purchase = require('../models/Purchase.js')(sequelize,Sequelize);
-db.purchaseItem = require('../models/PurchaseItem.js')(sequelize,Sequelize);
+
+db.user =           require('../models/User.js')(sequelize,Sequelize);
+db.item =           require('../models/Item.js')(sequelize,Sequelize);
+db.orderItem =      require('../models/OrderItem.js')(sequelize,Sequelize);
+db.storage =        require('../models/Storage.js')(sequelize,Sequelize);
+db.order =          require('../models/Order.js')(sequelize,Sequelize);
+db.pos =            require('../models/POS.js')(sequelize,Sequelize);
+db.location =       require('../models/Location.js')(sequelize,Sequelize);
+db.storageItem =    require('../models/StorageItem.js')(sequelize,Sequelize);
+db.table =          require('../models/Table.js')(sequelize,Sequelize);
+db.vat =            require('../models/VAT.js')(sequelize,Sequelize);
+db.purchase =       require('../models/Purchase.js')(sequelize,Sequelize);
+db.purchaseItem =   require('../models/PurchaseItem.js')(sequelize,Sequelize);
 
 db.location.hasOne(db.storage);
 db.storage.belongsTo(db.location);
