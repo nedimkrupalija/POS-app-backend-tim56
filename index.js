78c17073622203da406c814e6925f5c3d4962ee8
zlendo1 <zlendo1@etf.unsa.ba>
2024-04-13 14:54:52 +0200

    removed unnecessary imports

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const session = require('express-session');
-const cors = require('cors');
-
 const app = require('./config/app.js');
 const db = require('./config/db.js');
 
 db.sequelize.sync();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
