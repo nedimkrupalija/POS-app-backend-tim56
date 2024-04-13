41c29abc85fafdcbb2e43dcb3ad2f8296ab3bd92
zlendo1 <zlendo1@etf.unsa.ba>
2024-04-13 03:22:21 +0200

    moved common components to app.js

--- tests/test-app/test.app.js
+++ tests/test-app/test.app.js
@@ -1,35 +1,16 @@
-const express = require("express");
 const session = require("express-session");
 
-const adminRoutes = require("../../routes/adminRoutes.js");
-const authRoutes = require("../../routes/authRoutes.js");
-const orderRoutes = require("../../routes/orderRoutes.js");
-const authMiddleware = require("../../middleware/authMiddleware.js");
-const itemRoutes = require("../../routes/itemRoutes.js");
-const locationRoutes = require("../../routes/locationRoutes.js");
-const storageRoutes = require("../../routes/storageRoutes.js");
-const vatRoutes = require("../../routes/vatRoutes.js");
-const purchaseRoutes = require("../../routes/purchaseRoutes.js");
+const app = require("../../config/app.js");
 
-const app = express();
-app.use(express.json());
 app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: "secret",
-    cookie: {
-      secure: false,
-    },
-  })
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: "secret",
+        cookie: {
+            secure: false,
+        },
+    })
 );
-app.use("/auth", authRoutes);
-app.use("/admin", authMiddleware.verifyJWT, adminRoutes);
-app.use("/item", authMiddleware.verifyJWT, itemRoutes);
-app.use("/orders", authMiddleware.verifyJWT, orderRoutes);
-app.use("/location", authMiddleware.verifyJWT, locationRoutes);
-app.use("/storage", authMiddleware.verifyJWT, storageRoutes);
-app.use("/vat", authMiddleware.verifyJWT, vatRoutes);
-app.use("/purchase", authMiddleware.verifyJWT, purchaseRoutes);
 
 module.exports = app;
