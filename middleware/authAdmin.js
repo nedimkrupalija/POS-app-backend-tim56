5ad85520873d93063e290e1f0fc0685292537b20
zlendo1 <zlendo1@etf.unsa.ba>
2024-04-13 02:01:19 +0200

    middleware readability change

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,22 +1,18 @@
-
 const jwt = require('jsonwebtoken')
 
 const verifyAdmin = (req, res, next) => {
     try {
         const token = req.headers["authorization"]
         const decoded = jwt.decode(token);
-        if( decoded.role !== "superadmin" && decoded.role !== "admin"){
+
+        if(decoded.role !== "superadmin" && decoded.role !== "admin"){
             return res.status(403).json({ message: 'Forbidden' });
-        } 
-        next()
+        }
+
+        next();
     } catch (error) {
-       return res.status(401).json({
-        message: 'Auth failed'
-        })
+       return res.status(401).json({ message: 'Auth failed' });
     }
 }
 
-
 module.exports = verifyAdmin;
-
-
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,39 +1,38 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-
-
-const JWT_SECRET = require('../constants.js').JWT_SECRET
+const JWT_SECRET = require('../constants.js').JWT_SECRET;
 
 const verifyJWT = (req, res, next) => {
     try {
-        const token = req.headers["authorization"]
-        const decoded = jwt.verify(token, JWT_SECRET)
+        const token = req.headers["authorization"];
+        const decoded = jwt.verify(token, JWT_SECRET);
+
         if (decoded.role === 'admin' || decoded.role === "superadmin") { //extend jwt expiration time for 30 minutes
-            const extendedToken = jwt.sign({ ...decoded, exp: decoded.exp + (30 * 60) }, JWT_SECRET)
+            const extendedToken = jwt.sign({ ...decoded, exp: decoded.exp + (30 * 60) }, JWT_SECRET);
             
-            req.headers["Authorization"] = extendedToken
+            req.headers["Authorization"] = extendedToken;
         }
-        req.userData = decoded
-        next()
+
+        req.userData = decoded;
+
+        next();
     } catch (error) {
-        return res.status(401).json({
-        message: 'Auth failed'
-        })
+        return res.status(401).json({ message: 'Auth failed' });
     }
 }
 
 const addJwtHeader = (req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+
     res.setHeader('Access-Control-Expose-Headers', 'Authorization');
-        res.setHeader('Authorization', `${req.headers["Authorization"]}`);
-        next();
+
+    res.setHeader('Authorization', `${req.headers["Authorization"]}`);
+
+    next();
 };
 
 
 module.exports = {
     verifyJWT,
     addJwtHeader
-}
-
-
-
+};
