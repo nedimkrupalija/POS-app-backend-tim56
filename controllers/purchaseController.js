77c8d8159e947696b3aa0eda197e622c7ced8c56
aherak2 <aherak2@etf.unsa.ba>
2024-05-01 15:51:07 +0200

    printing added

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -1,5 +1,7 @@
 const db = require('../config/db.js');
-
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const Location = db.location
 const { generateServerErrorResponse } = require('../utils/messages.js');
 
 const Item = db.item
@@ -168,4 +170,85 @@ async function getPurchaseOrderByLocationId(req,res){
     }
 }
 
-module.exports = {createPurchaseOrder,getAllPurchaseOrders,getPurchaseOrderById,updatePurchaseOrder,deletePurchaseOrder, updatePurchaseStatus, getPurchaseOrderByLocationId};
+async function createInvoice(req, res) {
+    const tableData = req.body.tableData;
+
+    const doc = new PDFDocument();
+
+
+    function drawHorizontalLine(character) {
+        const lineWidth = doc.page.width ; 
+  const lineSymbol = "-".repeat(Math.floor(lineWidth / 8));
+  doc.text(lineSymbol, { align: 'left' }); 
+}
+    doc.pipe(res);
+
+    res.setHeader('Content-Disposition', 'attachment; filename="invoice.pdf"');
+    res.setHeader('Content-Type', 'application/pdf');const locations = await Location.findByPk(tableData.LocationId);
+    console.log(locations['name']);
+    
+    
+    doc.font('Courier')
+       .fontSize(10)
+       .text(locations['name'], { align: 'center' })
+       .moveDown(1);
+    
+    drawHorizontalLine("-");
+    doc.moveDown(2)
+    doc.font('Helvetica-Bold')
+       .fontSize(20)
+       .text('Invoice', { align: 'center' })
+      
+       doc.font('Courier')
+       .fontSize(10)
+       .text(`LID: ${tableData.id}`)
+       .moveDown(0.5);
+       if(tableData.tableId!=null)
+   doc.font('Courier')
+    .fontSize(10)
+    .text("TID: "+tableData.tableId)
+    drawHorizontalLine("-");
+
+    let yOffset = 175; 
+    tableData.items.forEach(item => {
+      const unitPrice  = item.sellingPrice;
+      const name=item.name;
+      const quantity=item.PurchaseItem.quantity;
+
+      const totalPrice = quantity * unitPrice;
+    
+      doc.text(name, 75, yOffset);
+      yOffset += 10;
+      doc.text(quantity.toString()+",00x", 75, yOffset);
+      doc.text(unitPrice.toFixed(2), 200, yOffset, { width: 100, align: 'right' });
+      doc.text(totalPrice.toFixed(2), 400, yOffset, { width: 100, align: 'right' });
+    
+      yOffset += 20;
+    });
+    
+
+    doc.moveDown(1);
+    doc.text("",75,yOffset);
+    drawHorizontalLine("-");
+
+       doc.moveDown(1).
+       font('Courier')
+       .text(`Total without VAT: `,75,doc.y, { align: 'left' });
+       doc.font('Courier').text(`${tableData.grandTotal-tableData.vat}`,400,doc.y,{align:'right'}).moveDown(1);
+       doc.font('Courier')
+       .text(`VAT: `,75,doc.y, { align: 'left' });
+       doc.font('Courier').text(`${tableData.vat}`,400,doc.y,{align:'right'}).moveDown(1);
+       doc.font('Courier')
+       .text(`Total: `,75,doc.y,{ align: 'left' });
+       doc.font('Courier').text(`${tableData.grandTotal}`,400,doc.y,{align:'right'}).moveDown(1);
+
+    doc.text("",75,doc.y);
+    drawHorizontalLine("-");
+    doc.moveDown(0.001);
+    drawHorizontalLine("-");
+    doc.moveDown(1).text("Thank you for your purchase!",{align:'center'}).moveDown(1);
+    drawHorizontalLine("-");
+
+    doc.end();
+}
+module.exports = {createPurchaseOrder,getAllPurchaseOrders,getPurchaseOrderById,updatePurchaseOrder,deletePurchaseOrder, updatePurchaseStatus, getPurchaseOrderByLocationId,createInvoice};
