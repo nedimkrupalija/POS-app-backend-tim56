6054e4119a05244e717c9aa50f227a5abac10184
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-05-08 21:23:26 +0200

    db data

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,10 @@
 const JWT_SECRET="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 const DB_CRED = {
-    name: "db_aa6c5b_sipos",
-    user: "aa6c5b_sipos",
-    pass: "sipostim56",
-    host: "MYSQL6008.site4now.net"
+    name: "sipos",
+    user: "root",
+    pass: "password",
+    host: "127.0.0.1:3306"
 }
 
 module.exports = {
