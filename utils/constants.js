2f0a68a5e62d3c6e3663fb8be26f5b09edb8a99c
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-05-09 01:14:45 +0200

    db info

--- utils/constants.js
+++ utils/constants.js
@@ -2,9 +2,9 @@ const JWT_SECRET="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 
 const DB_CRED = {
     name: "sipos",
-    user: "root",
-    pass: "password",
-    host: "127.0.0.1:3306"
+    user: "admin",
+    pass: "",
+    host: "localhost"
 }
 
 module.exports = {
