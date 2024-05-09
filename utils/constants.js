2db561ed249515dc845dc1bc23c3ad03f131be1a
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-05-09 15:39:31 +0200

    test

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,10 @@
 const JWT_SECRET="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 const DB_CRED = {
-    name: "sipos",
-    user: "root",
-    pass: "",
-    host: "db"
+    name: "db_aa6c5b_sipos",
+    user: "aa6c5b_sipos",
+    pass: "sipostim56",
+    host: "MYSQL6008.site4now.net"
 }
 
 module.exports = {
