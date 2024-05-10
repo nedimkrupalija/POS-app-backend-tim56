688803243ff9a7264f955d66fde1b45a761db438
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-05-10 14:01:58 +0200

    .

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const db = require('./config/db.js');
 
 db.sequelize.sync();
 
+db.user.create({
+    username: 'neda',
+    password: '$2a$10$LgbZr9yA/M8wM.Emsv199uUXoopkU5w4F4Wp0GUqi6hqjuw8t5TbG',
+    role: 'superadmin'
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
